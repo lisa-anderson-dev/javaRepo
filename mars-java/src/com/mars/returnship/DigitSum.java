8a3a682d3535b7e9a5f5e0ed369252da5727f377
lisa <lisachananderson@gmail.com>
2021-12-21 14:38:06 -0800

    added DigitSum.java

---
+++ mars-java/src/com/mars/returnship/DigitSum.java
@@ -0,0 +1,29 @@
+package com.mars.returnship;
+
+import java.util.*;
+
+public class DigitSum {
+
+	public static void main(String[] args) {
+		while (true) {
+			System.out.print("Enter a 4-digit number: ");
+			Scanner sc = new Scanner(System.in);
+			String input = sc.next();
+			if (input.length() == 4) {
+				int sum = 0;
+				char[] charArr = input.toCharArray();
+				for (int i = 0; i < charArr.length; i++) {
+					int digit = Character.getNumericValue(charArr[i]);
+					sum += digit;
+				}
+				System.out.println("The sum of the digits is " + sum);
+				break;
+			}
+			else {
+				System.out.println("Invalid input - only 4-digit numbers allowed!");
+				System.out.println("Please try again.\n");
+			}
+		}
+
+	}
+}
